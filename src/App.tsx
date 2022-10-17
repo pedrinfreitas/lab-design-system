83f60ef68b3e4c23608d573084c171adf3f5b30c
Pedro Freitas <pedrinfreitas@gmail.com>
2022-10-17 09:56:06 -0300

    feat: storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import "./styles/global.css";
 
 export function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-2xl font-bold underline">Hello world!</h1>
+      <button className="bg-cyan-500 font-medium px-4 py-2 m-2  rounded text-white hover:bg-cyan-300">
+        enviar
+      </button>
+    </>
+  );
 }
