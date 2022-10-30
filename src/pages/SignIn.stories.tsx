4ffe337e4e88ddec02da21ae932385df1c27ca2f
Pedro Freitas <pedrinfreitas@gmail.com>
2022-10-30 19:56:38 -0300

    feat: mockServiceWorker

--- src/pages/SignIn.stories.tsx
+++ src/pages/SignIn.stories.tsx
@@ -1,13 +1,28 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { within, userEvent, waitFor } from "@storybook/testing-library";
 import { expect } from "@storybook/jest";
+
+import { rest } from "msw";
 import { SignIn } from "./SignIn";
 
 export default {
-  title: "Components/Sign In",
+  title: "Pages/Sign In",
   component: SignIn,
   args: {},
   argTypes: {},
+  parameters: {
+    msw: {
+      handlers: [
+        rest.post("/session", (req, res, ctx) => {
+          return res(
+            ctx.json({
+              message: "Login realizado!",
+            })
+          );
+        }),
+      ],
+    },
+  },
 } as Meta;
 
 export const Default: StoryObj = {
